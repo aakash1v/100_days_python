870d5e6f729eaf0db548e452ad99ad6102b218fa
Aakash Kumar <aakashkumar113450@gmail.com>
2024-07-04 22:01:38 +0530

    day20 completed

---
+++ day20/main.py
@@ -0,0 +1,31 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(height=600,width=600)
+screen.bgcolor('black')
+screen.title('MY Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,'Up')
+screen.onkey(snake.down,'Down')
+screen.onkey(snake.right,'Right')
+screen.onkey(snake.left,'Left')
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(.1)
+    
+    snake.move()
+    
+
+
+    
+
+screen.exitonclick()
