329e08859ac70708dcb7a0a785dd913535b1c903
Aakash Kumar <aakashkumar113450@gmail.com>
2024-07-25 12:35:53 +0530

    trying smt sending random quote to my class mates...

--- day32/code3.py
+++ day32/code3.py
@@ -1,9 +1,26 @@
 import pandas
+import smtplib
+import random
+
+
+my_email = 'aakashkumarpy@gmail.com'
+password = 'fssbfleiunrdutff'
+
 
 data = pandas.read_csv('internship_student.csv',index_col=0)
+data_dict = data['Email'].to_dict()
 
-print(type(data))
-print(data.columns)
-print(data['Email '])zzz...
-fuckndfnaklsfhajf;k
+with open('quotes.txt') as file:
+    data = file.readlines()
+    quote =random.choice(data)
+
+#sending email...
+for key,value in data_dict.items():
+    print(value)
+    print(type(value))
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user = my_email,password = password)
+        connection.sendmail(from_addr = my_email, to_addrs =value,msg=
+                            f"Subject:Hi, {key}\n\nA quote for u.\n\n{quote}")
 
--- day32/main3.py
+++ day32/main3.py
@@ -19,7 +19,9 @@ today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
 if today_tuple in birthdays_dict:
+print(data['Email'])
     birthday_person = birthdays_dict[today_tuple]
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
